a0cd09fc8423285227edcf1aea35b08be4900a4e
indranil <prince.indranil@gmail.com>
2023-03-12 14:10:02 +0530

    Added top and left frames in the game GUI

---
+++ main.py
@@ -0,0 +1,30 @@
+from tkinter import *
+import settings
+import utils
+
+root = Tk()
+
+#Override the settings of the window
+root.configure(bg="black")
+root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
+root.title("Minesweeper Game")
+root.resizable(False, False)
+
+top_frame = Frame(
+    root,
+    bg = "red", #change later to black
+    width=settings.WIDTH,
+    height=utils.height_prct(25)
+)
+top_frame.place(x=0, y=0)
+
+left_frame = Frame(
+    root,
+    bg = "blue", #change later to black
+    width=utils.width_prct(25),
+    height=utils.height_prct(75)
+)
+left_frame.place(x=0, y=utils.height_prct(25))
+
+# run the window
+root.mainloop()
